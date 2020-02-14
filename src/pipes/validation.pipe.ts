424a2faff5c9a5462fc9d43b5db8162adf055e10
Mike Brits <mike.brits@gmail.com>
2020-02-14 20:15:24 +0000

    Add custom validation error response

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -16,7 +16,12 @@ export class ValidationPipe implements PipeTransform<any> {
     const object = plainToClass(metatype, value);
     const errors = await validate(object);
     if (errors.length > 0) {
-      throw new BadRequestException(errors);
+      console.log(errors);
+      throw new BadRequestException(
+        errors.map(({ property, constraints, value }) => ({
+          [property]: { valueProvided: value, constraints },
+        })),
+      );
     }
     return value;
   }
