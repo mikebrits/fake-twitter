7b59dfdf754ee64c46883bd53dad08bc69816146
Mike Brits <mike.brits@gmail.com>
2020-02-14 19:13:49 +0000

    Add validation to users. Add Tweet controller.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { TweetController } from './tweet/tweet.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
+  controllers: [AppController, UsersController, TweetController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,21 +1,32 @@
-import { IsString, IsNumber, IsBoolean, IsOptional } from 'class-validator';
-import { Optional } from '@nestjs/common';
+import {
+  IsString,
+  IsNumber,
+  IsBoolean,
+  IsOptional,
+  Length,
+  Min,
+  IsEmail,
+} from 'class-validator';
 
 export class CreateUserDTO {
   @IsOptional()
   @IsString()
+  @Length(3, 50)
   name: string;
 
-  @IsString()
+  @IsString({ message: 'User handle not provided' })
+  @Length(3, 15)
   handle: string;
 
-  @IsString()
+  @IsString({ message: 'User email not provided' })
+  @IsEmail()
   email: string;
 
   @IsOptional()
   @IsNumber()
+  @Min(13)
   age: number;
 
-  @IsBoolean()
+  @IsBoolean({ message: 'User must be identified as over 18 or not' })
   over18: boolean;
 }
